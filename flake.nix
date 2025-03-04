73aca1153bab9a86da7563bcd8b701db066efc06
Qiming Chu <cchuqiming@gmail.com>
2025-03-04 16:45:04 +0800

    chore: replace flake-utils with flake-parts and update flake configuration
    This commit replaces the `flake-utils` dependency with `flake-parts` and
    updates the flake configuration to use the new `flake-parts` structure.
    The `utils.lib.eachDefaultSystem` function is replaced with
    `parts.lib.mkFlake`, and the `systems` list is explicitly defined.
    Additionally, the `deps` list is removed, and build inputs are directly
    specified in the `devShells.default` configuration.
    
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,53 +1,62 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    {
+    inputs@{
       self,
       nixpkgs,
-      utils,
+      parts,
       treefmt-nix,
       ...
     }:
-    utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        treefmtEval = treefmt-nix.lib.evalModule pkgs {
-          projectRootFile = "Cargo.toml";
-          programs = {
-            nixfmt.enable = true; # nix
-            rustfmt.enable = true; # rust
-            yamlfmt.enable = true; # yaml
+    parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        treefmt-nix.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        {
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          treefmtEval = treefmt-nix.lib.evalModule pkgs {
+            projectRootFile = "Cargo.toml";
+            programs = {
+              nixfmt.enable = true; # nix
+              rustfmt.enable = true; # rust
+              yamlfmt.enable = true; # yaml
+            };
+          };
+        in
+        {
+          packages = {
+            git-commit-generator = pkgs.callPackage ./nix/pkgs/git-commit-generator.nix { };
+            default = self.packages.${system}.git-commit-generator;
+          };
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.git-commit-generator ];
+            buildInputs = with pkgs; [
+              rust-analyzer
+              rustfmt
+            ];
+            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+            PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+          };
+          formatter = treefmtEval.config.build.wrapper;
+          checks = {
+            formatting = treefmtEval.config.build.check self;
           };
         };
-        deps = with pkgs; [
-          cargo
-          rust-analyzer
-          rustfmt
-          openssl
-        ];
-      in
-      {
-        packages = {
-          git-commit-generator = pkgs.callPackage ./nix/pkgs/git-commit-generator.nix { };
-          default = self.packages.${system}.git-commit-generator;
-        };
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ pkg-config ];
-          buildInputs = [ deps ];
-          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
-          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-        };
-        formatter = treefmtEval.config.build.wrapper;
-        checks = {
-          formatting = treefmtEval.config.build.check self;
-        };
-      }
-    );
+    };
 }
