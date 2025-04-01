c418876cdba539a7109ada92da5d11c884b6b1f2
Qiming Chu <cchuqiming@gmail.com>
2025-04-01 20:18:07 +0800

    build(nix): build static package in nix
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

--- nix/pkgs/git-commit-generator.nix
+++ nix/pkgs/git-commit-generator.nix
@@ -1,12 +1,14 @@
 {
   pkgs,
-  rustPlatform,
   pkg-config,
-  openssl,
-  rustPackages,
-  ...
+  stdenv,
+  lib,
 }:
-rustPlatform.buildRustPackage {
+
+let
+  libiconvLib = pkgs.pkgsStatic.libiconv.dev;
+in
+pkgs.pkgsStatic.rustPlatform.buildRustPackage {
   pname = "git-commit-generator";
   version = "0.1.0.0";
 
@@ -14,12 +16,22 @@ rustPlatform.buildRustPackage {
     lockFile = ./../../Cargo.lock;
   };
   src = ./../..;
-  PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+
+  PKG_CONFIG_PATH = "${pkgs.pkgsStatic.openssl.dev}/lib/pkgconfig";
+
   nativeBuildInputs = [
     pkg-config
   ];
+
   buildInputs = [
-    rustPackages.clippy
-    openssl
+    pkgs.pkgsStatic.openssl.dev
   ];
+
+  RUSTFLAGS = lib.optionalString stdenv.hostPlatform.isDarwin ''
+    -C link-args=-L${libiconvLib}/lib
+  '';
+
+  NIX_LDFLAGS = lib.optionalString stdenv.hostPlatform.isDarwin ''
+    -L${libiconvLib}/lib -liconv -lcharset
+  '';
 }
